d15e28258ee5973bb9c350786c5e8123f64ef763
Shreyash Bhandare <s2r0bcoding@gmail.com>
2025-09-12 00:21:06 +0530

    quicksort

---
+++ Step 2 - Learn Imp Sorting Techniques/quick.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high){
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while(i < j){
+        while(arr[i] <= pivot && i <= high-1) i++;
+        while(arr[j] >= pivot && j >= low+1) j--;
+        if(i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quick(vector<int> &arr, int low, int high){
+    if(low < high){
+        int part = partition(arr, low, high);
+        quick(arr, low, part-1);
+        quick(arr, part+1, high);
+    }
+}
+
+int main(){
+    vector<int> q = {13, 46, 24, 52, 20, 9};
+    quick(q, 0, q.size()-1);
+    for(int i : q){
+        cout<<i<<" ";
+    }
+    return 0;
+}
